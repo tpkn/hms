6816a193c037241edc338d59b689c2f61ff8a2b3
tpkn <tpkn@users.noreply.github.com>
2021-05-28 01:26:58 +0300

    Add files via upload

--- hms.go
+++ hms.go
@@ -13,17 +13,17 @@ type timestamp struct {
 	msec    float64
 }
 
+func (t timestamp) Stringify() string {
+	return fmt.Sprintf("%02v:%02v:%02v.%03v", t.hours, t.minutes, t.seconds, t.msec)
+}
+
 func Seconds(duration float64) string {
-	ts := timestamp{
+	t := timestamp{
 		math.Floor(duration / 3600),
 		math.Floor(math.Mod(duration / 60, float64(60))),
 		math.Floor(math.Mod(duration, float64(60))),
 		math.Floor(math.Mod(duration * 1000, 1000)),
 	}
 	
-	return stringify(ts)
-}
-
-func stringify(t timestamp) string {
-	return fmt.Sprintf("%02v:%02v:%02v.%03v", t.hours, t.minutes, t.seconds, t.msec)
+	return t.Stringify()
 }
